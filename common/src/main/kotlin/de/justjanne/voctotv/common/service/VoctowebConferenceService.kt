df65bb8bfbce6871c96462aa6c2cf0b0db5a4f7b
Janne Mareike Koschinski <janne@kuschku.de>
2026-08-16 15:55:43 +0200

    feat: cache banner image results for max 24h

--- common/src/main/kotlin/de/justjanne/voctotv/common/service/VoctowebConferenceService.kt
+++ common/src/main/kotlin/de/justjanne/voctotv/common/service/VoctowebConferenceService.kt
@@ -2,8 +2,8 @@ package de.justjanne.voctotv.common.service
 
 import de.justjanne.voctotv.voctoweb.api.VoctowebApi
 import de.justjanne.voctotv.voctoweb.model.ConferenceModel
+import java.time.LocalDate
 import javax.inject.Inject
-import kotlin.collections.map
 
 class VoctowebConferenceService
     @Inject
@@ -38,6 +38,11 @@ class VoctowebConferenceService
             const val Size = 384
             const val BaseUrl = "https://voctotv-assets.s3.kuschku.de"
 
-            fun bannerUrl(id: String): String = "$BaseUrl/$id/card-$Size.png"
+            private val version =
+                LocalDate.now().apply {
+                    "%04d%02d%02d".format(year, monthValue, dayOfMonth)
+                }
+
+            fun bannerUrl(id: String): String = "$BaseUrl/$id/card-$Size.png?v=$version"
         }
     }
